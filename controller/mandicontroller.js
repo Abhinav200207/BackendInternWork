c9fd5ab06bde60160f06a05ee16908a5748746c3
Abhinav <abhinav.maurya202@gmail.com>
2022-06-10 13:50:38 +0530

    Some bugs fixed commit

--- controller/mandicontroller.js
+++ controller/mandicontroller.js
@@ -17,11 +17,11 @@ exports.updateProduct = async (req, res) => {
     try {
         const product = await Mandi.findOne({ marketID: req.body.reportDetails.marketID, cmdtyID: req.body.reportDetails.cmdtyID });
         let list = product.users;
-        if (!(req.body.reportDetails.userId in product.users)) {
+        let price = (product.users.length * product.price + req.body.reportDetails.price / req.body.reportDetails.convFctr) / (product.users.length + 1);
+        console.log(`(${product.users.length}*${product.price}+${req.body.reportDetails.price / req.body.reportDetails.convFctr})/${product.users.length} + 1 = ${price}`);
+        if (product.users.indexOf(req.body.reportDetails.userId) === -1) {
             list.push(req.body.reportDetails.userId);
         }
-        let price = (product.users.length * product.price + req.body.reportDetails.price / req.body.reportDetails.convFctr) / (product.users.length + 1)
-        console.log(price)
         const newProduct = {
             cmdtyName: req.body.reportDetails.cmdtyName,
             cmdtyID: req.body.reportDetails.cmdtyID,
@@ -32,14 +32,11 @@ exports.updateProduct = async (req, res) => {
             priceUnit: "kg",
             price: price
         }
-        const updatedProduct = await Mandi.findByIdAndUpdate(product._id, newProduct, {
-            new: true,
-            runValidator: true,
-            useFindAndModify: false
-        })
+        console.log(newProduct);
+        const updatedProduct = await Mandi.findByIdAndUpdate(product._id, newProduct)
         res.status(201).json({
             success: true,
-            product: product
+            id: product._id
         })
     } catch (error) {
         res.status(500).json({
